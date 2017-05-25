c6cce2a2263f6aca0edfce67cccb03cca83a4c23
Shohei Aoki <shoaok@gmail.com>
2017-05-25 16:12:53 +0900

    initial commit

---
+++ fem.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def simple_fem():
+    initialize()
+    make_D()
+    make_B()
+    make_Ke()
+    make_K()
+    set_boundary_condition()
+    solve()
+    make_reaction()
+    make_strain_element()
+    make_stress_element()
+
+def initialize():
+    pass
+def make_D():
+    pass
+def make_B():
+    pass
+def make_Ke():
+    pass
+def make_K():
+    pass
+def set_boundary_condition():
+    pass
+def solve():
+    pass
+def make_reaction():
+    pass
+def make_strain_element():
+    pass
+def make_stress_element():
+    pass
+
+
+if __name__=="__main__":
+    simple_fem()
+
